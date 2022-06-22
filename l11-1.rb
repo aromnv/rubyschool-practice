8b3cf3378457257d45a0668e33c7e57bc2494318
aromnv <antonromanov343@gmail.com>
2022-06-22 22:45:10 +0300

    remove folder

---
+++ l11-1.rb
@@ -0,0 +1,48 @@
+print "(R)ock, (S)cissors, (P)aper? "
+s = gets.strip.capitalize
+
+if s == "R"
+  user_choice = :rock
+elsif s == "S"
+  user_choice = :scissors
+elsif s == "P"
+  user_choice = :paper
+else
+  puts "Can't understand what you want, sorry..."
+  exit
+end
+
+arr = [:rock, :scissors, :paper]
+
+computer_choice = arr[rand(0..2)]
+puts "Your choice: #{user_choice}"
+puts "Computer choice: #{computer_choice}"
+
+
+matrix = [
+  [:rock, :rock, :draw],
+  [:scissors, :scissors, :draw],
+  [:paper, :paper, :draw],
+
+  [:rock, :scissors, :first_win],
+  [:rock, :paper, :second_win],
+
+  [:scissors, :rock, :second_win],
+  [:scissors, :paper, :first_win],
+
+  [:paper, :rock, :first_win],
+  [:paper, :scissors, :second_win]
+]
+
+matrix.each do |item|
+  if item[0] == user_choice && item[1] == computer_choice
+    if item[2] == :first_win
+      puts "User wins!"
+    elsif item [2] == :second_win
+      puts "Computer wins!"
+    else
+      puts "Draw!"
+    end
+    exit
+  end
+end
